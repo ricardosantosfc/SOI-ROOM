3a57620c2039cb082e784c147a657bcb1a155785
ricardosantosfc <58186574+ricardosantosfc@users.noreply.github.com>
2026-02-12 23:17:50 +0000

    added physics debug + rigid body tryout

--- react-three-fiber-test/src/components/Experience.tsx
+++ react-three-fiber-test/src/components/Experience.tsx
@@ -1,6 +1,7 @@
 import { Center, Environment, Sky, useGLTF } from "@react-three/drei";
 import { Physics } from "@react-three/rapier";
 import { Controls } from "../Controls";
+import { Model } from "../Room";
 
 export const Experience = () => {
 
@@ -8,17 +9,15 @@ export const Experience = () => {
 
     return (
         <>
-            <Physics gravity={[0, 0, 0]}>
+            <Physics gravity={[0, 0, 0]} debug>
+                
                 <Controls />
                 <Center>
-                    <group >
-                        
-                        <primitive object={gltf.scene} />
-                    </group>
+                      <Model />
                 </Center>
                 <Environment preset="sunset" />
                 <ambientLight intensity={0.4} />
-                <directionalLight position={[5, 5, 5]} intensity={0.4} castShadow />
+                <directionalLight position={[5, 5, 5]} intensity={0.4} />
                 <Sky inclination={0.52} />
             </Physics>
         </>
