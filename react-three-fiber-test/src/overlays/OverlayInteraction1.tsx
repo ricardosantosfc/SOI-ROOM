7f584db22f6cc55b49cbbb88c72c9829add52e69
ricardosantosfc <58186574+ricardosantosfc@users.noreply.github.com>
2026-02-27 01:28:14 +0000

    refactored book overlay page moving logic to stepped input

--- react-three-fiber-test/src/overlays/OverlayInteraction1.tsx
+++ react-three-fiber-test/src/overlays/OverlayInteraction1.tsx
@@ -10,7 +10,7 @@ const informations = [
     name: "Lucky",
     mediumYear: "Charcoal, 2021 "
   },
-   {
+  {
     name: "Musashi (Takehiko Inoue study)",
     mediumYear: "Ink, watercolor, and gouache, 2016 "
   },
@@ -22,7 +22,7 @@ const informations = [
     name: "Fräulein Bürstner",
     mediumYear: "Watercolor, 2021 "
   },
- 
+
   {
     name: "Musashi (Takehiko Inoue study)",
     mediumYear: "Ink and digital, 2016 "
@@ -65,27 +65,39 @@ export const OverlayInteraction1 = () => {
 
   })),)
 
+  const changePage = (value:number )=>{
+    if((page + value>=0) && (page + value <= pictures.length/2)){
+      setPage(page+value)
+    }
+  }
+
   return (
     <>
       <main className={styles.main} /*className=" pointer-events-none select-none z-10 fixed  inset-0  flex justify-between flex-col" */ >
 
         <div className={styles.controls} /*className="w-full overflow-auto pointer-events-auto flex justify-center"*/>
-          <div className="overflow-auto flex items-center gap-4 max-w-full p-10">
-            {[...pages].map((_, index) => (
-              <button
-                key={index}
-                className={`border-transparent hover:border-white transition-all duration-300  px-4 py-3 rounded-full  text-lg uppercase shrink-0 border ${index === page
-                  ? "bg-white/90 text-black"
-                  : "bg-black/30 text-white"
-                  }`}
-                onClick={() => setPage(index)}
-              >
-                {index === 0 ? "Cover" : `Pages ${index * 2 - 1} – ${index * 2}`}
-              </button>
-            ))}
+          <button className={styles.buttonControl} onClick={() => changePage(-1)}>
+            <img src= "../arrow-left.svg"></img>
+          </button>
+          <input className={styles.pageInput}
+            value={page}
+            onChange={(e) => {
+              const value = e.target.valueAsNumber;
 
-
-          </div>
+              if (Number.isNaN(value)) {
+                setPage(0);
+                return;
+              }
+              const clamped = Math.min(2, Math.max(0, value));
+              setPage(clamped);
+            }}
+            type="number"
+            max={pictures.length / 2}
+            min={0}
+          ></input>
+          <button className={styles.buttonControl} onClick={() => changePage(1)}>
+            <img src= "../arrow-right.svg"></img>
+          </button>
         </div>
 
       </main>
@@ -99,9 +111,9 @@ export const OverlayInteraction1 = () => {
           <h3>{informations[(page - 1) * 2 + 1].mediumYear}</h3>
         </div>
       </div>)}
-       {page !== 0 &&(<button className="btn"
+      {page !== 0 && (<button className="btn"
         onClick={() => setIsInfoHidden(!isInfoHidden)}
-      > <img className ="btn-img" src = {isInfoHidden? "../chevron-down.svg" : "../chevron-up.svg"}></img></button>)}
+      > <img className="btn-img" src={isInfoHidden ? "../chevron-down.svg" : "../chevron-up.svg"}></img></button>)}
     </>
   );
 };
