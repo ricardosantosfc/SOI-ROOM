b3745a9a1572ec3728bd180af88b05d2b02df342
ricardosantosfc <58186574+ricardosantosfc@users.noreply.github.com>
2026-04-02 00:30:48 +0100

    experience, default env: added all final models, changed lighting to use of three roomEnv
    so it more closesly matches editor

---
+++ react-three-fiber-test/src/DefaultEnvironment.tsx
@@ -0,0 +1,25 @@
+import { useThree } from '@react-three/fiber'
+import { useEffect } from 'react'
+import * as THREE from 'three'
+import { RoomEnvironment } from 'three/addons/environments/RoomEnvironment.js'
+
+//match environment used by the online editor (uses exported RoomEnvironment)
+export function DefaultEnvironment() {
+  const { gl, scene } = useThree()
+
+  useEffect(() => {
+    const pmremGenerator = new THREE.PMREMGenerator(gl)
+    const envScene = new RoomEnvironment()
+
+    const envMap = pmremGenerator.fromScene(envScene).texture
+
+    scene.environment = envMap
+
+    return () => {
+      envScene.dispose()
+      pmremGenerator.dispose()
+    }
+  }, [gl, scene])
+
+  return null
+}
--- react-three-fiber-test/src/components/Experience.tsx
+++ react-three-fiber-test/src/components/Experience.tsx
@@ -1,23 +1,32 @@
 import { Center, Environment, Sky} from "@react-three/drei";
 import { Physics } from "@react-three/rapier";
 import { Player } from "../Player";
-import { Model } from "../Tree_spha1";
+import { Model } from "../Final_model";
 import { Book } from "./Book";
+import { DefaultEnvironment } from "../DefaultEnvironment";
+import { Skydome } from "../Skydome_only";
+//import { FakeShadow } from "../Shadow_text_decal";
+
 
 export const Experience = () => {
 
     return (
         <>
-            <Physics gravity={[0, 0, 0]} debug>
+            <Physics gravity={[0, 0, 0]}>
                 <Player />
                 <Book/>
                 <Center>
-                    <Model/>     
+                    <Skydome/>
+                    <Model/>    
+                    {/*<FakeShadow></FakeShadow>*/} 
                 </Center>
-                <Environment preset="sunset" />
+                <DefaultEnvironment></DefaultEnvironment>
+                {/*<hemisphereLight intensity={1} />
+                <directionalLight position={[5, 5, 5]} intensity={1} />*/}
+                {/*<Environment preset="city" />
                 <ambientLight intensity={0.4} />
                 <directionalLight position={[5, 5, 5]} intensity={0.4} />
-                <Sky inclination={0.52} />
+                <Sky inclination={0.52} />*/}
             </Physics>
         </>
     );
