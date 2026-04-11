b373eca5b50fec0b0e308735af5d6543dc93e9db
ricardosantosfc <58186574+ricardosantosfc@users.noreply.github.com>
2026-04-11 17:18:50 +0100

    added chevron button sfx

--- react-three-fiber-test/src/overlays/ChevronToggleButton.tsx
+++ react-three-fiber-test/src/overlays/ChevronToggleButton.tsx
@@ -1,5 +1,8 @@
 import { useStore } from "../store"
 import { useShallow } from "zustand/shallow"
+
+const audio = new Audio("/sfx/buttonclick-004.mp3");
+audio.volume= 0.7
 export function ChevronToggleButton() {
 
     const { isInfoHidden, setIsInfoHidden } = useStore(useShallow((state) => ({
@@ -9,8 +12,12 @@ export function ChevronToggleButton() {
 
     })),)
 
+    const handleClick =() =>{
+        audio.play();
+        setIsInfoHidden(!isInfoHidden);
+    }
     return (
-        <button className="btn-chevron-hide-toggle" onClick={() => setIsInfoHidden(!isInfoHidden)}>
+        <button className="btn-chevron-hide-toggle" onClick={handleClick}>
             <img className="btn-chevron-hide-toggle-img" src={isInfoHidden ? "../chevron-down.svg" : "../chevron-up.svg"}></img>
         </button>
     );
